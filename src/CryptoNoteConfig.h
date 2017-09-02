d0b91436bfabced829c269b463a70ea9d3e345e4
Jesse Jusoli <jessejusoli@gmail.com>
2017-09-02 19:09:41 +0000

    CryptoNoteConfig.h edited fix coin genesis

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "narutocoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "narutocoind";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
