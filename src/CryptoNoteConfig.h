553fcfd191996d65eee995b235ffb393242fd5fe
datacenter <jessejusoli@gmail.com>
2018-01-01 02:11:15 +0000

    Init kurumin

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "kurumin";
-const char GENESIS_COINBASE_TX_HEX[]                         = "";
+const char GENESIS_COINBASE_TX_HEX[]                         = "013c01ff0001fac484c69cd608029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017efdb6606fc47d34f37188bd4e3e03a555c769d0939179e0120fbeb67c8f91be";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
